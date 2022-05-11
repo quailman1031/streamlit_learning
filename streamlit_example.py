0dc1dcb4c3571c1bd9e49b9639028df60e5714d8
quailman1031 <82731239+quailman1031@users.noreply.github.com>
2022-05-11 10:41:36 -0500

    converted json file with block

--- streamlit_example.py
+++ streamlit_example.py
@@ -183,12 +183,13 @@ specimen_details = {'date_received':'04/01/2022',
                     'date_report': '05/01/2022',
                     'test_type': 'AOA'}
 
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
+#if uploaded_file:
+#    df = pd.read_csv(uploaded_file)
 
 if uploaded_file is not None:  
-    with open(uploaded_file.name) as json_file:
-        lab_results = json.load(json_file)
+    #with open(uploaded_file.name) as json_file:
+    #    lab_results = json.load(json_file)
+    lab_results = json.load( open(uploaded_file.name) )
     #lab_results = json.load(uploaded_file.name)
     patient_data["ID"] = lab_results['PATIENT']['id']
     patient_data["DOB"] = lab_results['PATIENT']['birthDate']
